8c978e25f2f279499af773c0385cb08e6c8d492f
Lalo Blanc <lalo@physicslab.space>
2025-06-26 22:43:12 -0500

    uncomment print_labels function call in main

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flow.barcode_logger import scan_barcodes
 from flow.spreadsheet import append_to_spreadsheet
+from flow.label_print import print_labels
 
 
 def main():
     entries = scan_barcodes()
     if entries:
         append_to_spreadsheet(entries)
-        # print_labels(entries)  # Uncomment when ready
+        print_labels(entries) 
 
 if __name__ == '__main__':
     main()
