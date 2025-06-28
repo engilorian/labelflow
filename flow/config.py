069e59d36a33cfcc453a14f682e8e1653f6dd576
Lalo Blanc <lalo@physicslab.space>
2025-06-27 21:05:37 -0500

    reorganize configuration settings for clarity and consistency

--- flow/config.py
+++ flow/config.py
@@ -1,3 +1,10 @@
+import os
 
-EXCEL_FILE = 'barcodes.xlsx'
-SHEET_NAME = 'Barcodes'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+LABEL_FILE = os.path.join(BASE_DIR, "template.label")
+PRINTER_NAME = "DYMO_LabelWriter"
+
+EXCEL_FILE = os.path.join(BASE_DIR, "barcodes.xlsx")
+SHEET_NAME = "Barcodes"
